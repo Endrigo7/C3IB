be69bb6701805a8e3d79b743fd371d998558ae4b
endrigo7@gmail.com <endrigo7@gmail.com>
2021-10-25 08:40:52 -0300

    ajuste

--- src/main/java/school/cesar/c3ib/repositories/ContaRepository.java
+++ src/main/java/school/cesar/c3ib/repositories/ContaRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class ContaRepository {
 
-    private List<Conta> contas;
+    private ArrayList<Conta> contas;
 
     public ContaRepository(){
         contas = new ArrayList<>();
